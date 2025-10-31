e15389cd59d1c864b8168f8d583fa189ba3d6b4f
magxalasinaye00-max <magxalasinaye00@gmail.com>
2025-10-31 09:26:25 +0200

    exercise 2

---
+++ Operations-types.js
@@ -0,0 +1,10 @@
+let number1 = 5;
+let number2 = 10;
+let number1String = number1.toString();
+let sum = number1 + number2
+let product = number1 * number2;
+console.log("number1:", number1);
+console.log("number2:", number2);
+console.log("number1String:", number1String);
+console.log("Sum:", sum);
+console.log("Product:", product);
